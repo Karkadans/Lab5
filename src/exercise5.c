765f6ebcf7ddeab50e55c3b098e914699cb60268
Karkadans <gleb.denerol@gmail.com>
2025-10-27 18:28:15 +0300

    готово

--- src/exercise5.c
+++ src/exercise5.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-    double A[10][3], B[3][10], C[10][10];
-    int i, j, k;
-    for(i = 0; i < 10; i++) {
-        for(j = 0; j < 3; j++) {
-            scanf("%lf", &A[i][j]);
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    int a[10][3];
+    int b[3][10];
+    int f[10][10];
+    int q = 0;
+    int s = 1;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 3; j++) {
+            a[i][j] = i + j + 1; s++;
         }
     }
-    for(i = 0; i < 3; i++) {
-        for(j = 0; j < 10; j++) {
-            scanf("%lf", &B[i][j]);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 10; j++) {
+        b[i][j] = i + j + 1; s++;
         }
     }
-    for(i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
-            C[i][j] = 0;
-            for(k = 0; k < 3; k++) {
-                C[i][j] += A[i][k] * B[k][j];
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            f[i][j] = 0;
+            for (int k = 0; k < 3; k++) {
+                f[i][j] += a[i][k] * b[k][j];
             }
         }
     }
-    for(i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
-            printf("%g", C[i][j]);
-            if (i < 9 || j < 9) printf(" ");
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            q++;
+            printf("%d ", f[i][j]);
         }
     }
-    printf("\n");
-    
-    return 0;
-}
+} 
