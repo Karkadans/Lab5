55238adb271fe2a7defc109a206f92608f0c83c0
Заварыкин Глеб ИП-514 <gleb.denerol@gmail.com>
2025-10-27 14:31:48 +0300

    2

--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+    double array[10];
+    for(int i = 0; i <10; ++i) {
+        scanf("%lf", &array[i]);
+    }
+for(int i =9; i>=0; i--) {
+    printf("%g", array[i]);
+    if (i>0) {
+        printf(" ");
+    }
+}
+printf("\n");
+return 0; 
 }
