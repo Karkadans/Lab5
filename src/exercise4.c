e3a2eada8a103f0888df33c56fd5e0dd3ce28019
Karkadans <gleb.denerol@gmail.com>
2025-10-27 17:18:32 +0300

    4

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    double a[10];
+    
+    for(int i = 0; i < 10; i++) scanf("%lf", &a[i]);
+    
+    double x = atof(argv[1]);
+    int k = atoi(argv[2]);
+    
+    for(int i = 9; i > k; i--) a[i] = a[i-1];
+    a[k] = x;
+    
+    for(int i = 0; i < 10; i++) printf(i < 9 ? "%g " : "%g\n", a[i]);
+    
     return 0;
-}
+}  
