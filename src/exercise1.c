fd2d343e5ea929d69a3df0e2706378bacb2b0853
Заварыкин Глеб ИП-514 <gleb.denerol@gmail.com>
2025-10-27 15:42:10 +0300

    1

--- src/exercise1.c
+++ src/exercise1.c
@@ -2,25 +2,14 @@
 
 int main(int argc, char** argv) {
     double array[10];
-    double sum = 0.0;
-    double average;
+    double sum =0.0;
+    double b;
     
-    
-    for(int i = 0; i < 10; ++i) {
-        scanf("%lf", &array[i]);
-        sum += array[i];
-    }
-    
-    average = sum / 10.0;
-    
-    
-    if (average >= 0) {
-        average = (int)(average * 100 + 0.5) / 100.0;
-    } else {
-        average = (int)(average * 100 - 0.5) / 100.0;
-    }
-    
-    printf("%.3f\n", average); 
-    
-    return 0;
+for(int i =0; i<10; ++i){
+    scanf("%lf", &array[i]);
+    sum += array[i];
 }
+b = sum/10.0;
+printf("%.3f\n", b);
+return 0;  
+}  
