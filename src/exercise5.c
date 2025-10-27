a1ed1f2976e21f98e85ec9757a2a6dcbc3a4aa92
Karkadans <gleb.denerol@gmail.com>
2025-10-27 18:19:28 +0300

    ого

--- src/exercise5.c
+++ src/exercise5.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char *argv[]) {
-    int q[10][3];
-    int w[3][10];
-    int e[10][10];
-    int t = 0;
-    int r = 1;
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 3; j++) {
-            q[i][j] = i + j + 1; r++;
+
+int main(int argc, char** argv) {
+    double A[10][3], B[3][10], C[10][10];
+    int i, j, k;
+    for(i = 0; i < 10; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%lf", &A[i][j]);
         }
     }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 10; j++) {
-        w[i][j] = i + j + 1; r++;
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 10; j++) {
+            scanf("%lf", &B[i][j]);
         }
     }
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            e[i][j] = 0;
-            for (int k = 0; k < 3; k++) {
-                e[i][j] += q[i][k] * w[k][j];
+    for(i = 0; i < 10; i++) {
+        for(j = 0; j < 10; j++) {
+            C[i][j] = 0;
+            for(k = 0; k < 3; k++) {
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            t++;
-            printf("%d ", e[i][j]);
+    for(i = 0; i < 10; i++) {
+        for(j = 0; j < 10; j++) {
+            printf("%g", C[i][j]);
+            if (i < 9 || j < 9) printf(" ");
         }
     }
-}  
+    printf("\n");
+    
+    return 0;
+}
