f2ce06834f048539b4b3fdb8c92a6bb042c5d98b
Заварыкин Глеб ИП-514 <gleb.denerol@gmail.com>
2025-10-27 14:22:06 +0300

    1

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+    double array[10];
+    double sum =0.0;
+    double b;
+    
+for(int i =0; i<10; ++i){
+    scanf("%lf", &array[i]);
+    sum += array[i];
 }
+b = sum/10.0;
+printf("%.3f\n", b);
+return 0;  
+} 
